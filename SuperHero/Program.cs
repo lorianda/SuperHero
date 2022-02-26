ae08e756c5b57fe401b877fcaba54782ecd005e1
lorianda <lorianda@inbox.lv>
2022-02-26 15:00:53 +0200

    Add program

---
+++ SuperHero/Program.cs
@@ -0,0 +1,30 @@
+﻿
+/*
+-Create variables:
+-Hero name - String
+- Hero age - int
+- Hero powers - heroPower1, heroPower2, heroPower3
+- Create "Hero Card" view.Print variables to console
+*/
+
+byte age;
+age = 33;
+string name = "lori";
+string powers1= "goodheart";
+string powers2 = "nice";
+string powers3 = "strongbeliever";
+Console.WriteLine(name);
+Console.WriteLine("HelloWorld");
+     Console.WriteLine("*************************************************");
+
+Console.WriteLine("Printing " + name + " Card:");
+
+Console.WriteLine("*************************************************");
+
+Console.WriteLine("Name is: " + name);
+
+Console.WriteLine("Age is: " + age);
+
+Console.WriteLine("Powers are: " + "(" + powers1 + ")" + " " + "(" + powers2 + ")" + " " + "(" + powers3 + ")");
+
+Console.WriteLine("*************************************************");
